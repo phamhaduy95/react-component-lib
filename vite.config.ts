a77ea2cb3a2311174ebcdfe0fe757e2f9dccc8d2
phamhaduy95 <ph.duy113@gmail.com>
2025-04-05 16:29:13 +0700

    set tailwind theme and add it to app

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 import { defineConfig } from 'vite';
-
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
+			'@styles': path.resolve(__dirname, './src/styles'),
 			'@components': path.resolve(__dirname, './src/components'),
 			'@pages': path.resolve(__dirname, './src/pages')
 		}
