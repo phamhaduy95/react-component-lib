52cf861addbbb2906786a590e9f401caa34e329b
phamhaduy95 <ph.duy113@gmail.com>
2025-04-05 16:32:16 +0700

    add pages directory

---
+++ src/pages/components/ComponentsPage.tsx
@@ -0,0 +1,40 @@
+import NavigationBar, { type NavigationBarProps } from '@components/NavigationBar';
+import { NavLink, Outlet } from 'react-router';
+
+const ComponentsPage = () => {
+	const links: NavigationBarProps['items'] = [
+		{
+			items: [
+				{ href: '/components/button', title: 'Button' },
+				{
+					href: '/components/navigation-bar',
+					title: 'NavigationBar'
+				},
+				{
+					href: '/components/scrollbar',
+					title: 'ScrollBar'
+				}
+			],
+			title: 'Components'
+		}
+	];
+
+	const CustomLink: NavigationBarProps['CustomLink'] = ({ href, title, onClick, disabled }) => {
+		return (
+			<NavLink to={href} onClick={onClick} aria-disabled={disabled} data-disabled={disabled}>
+				{title}
+			</NavLink>
+		);
+	};
+
+	return (
+		<div className="flex h-full w-full">
+			<NavigationBar className="w-52" items={links} CustomLink={CustomLink} />
+			<main className="">
+				<Outlet />
+			</main>
+		</div>
+	);
+};
+
+export default ComponentsPage;
---
+++ src/pages/components/button/ButtonPage.tsx
@@ -0,0 +1,7 @@
+import Button from '@components/Button';
+
+const ButtonPage = () => {
+	return <Button />;
+};
+
+export default ButtonPage;
