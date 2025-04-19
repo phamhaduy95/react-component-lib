008c55c847c8564a008c0421c458ec7b9b60ce66
phamhaduy95 <ph.duy113@gmail.com>
2025-04-19 11:25:56 +0700

    reconfigure Vite and tsc to allow generating d.ts for built bundle

--- vite.config.ts
+++ vite.config.ts
@@ -3,31 +3,45 @@ import react from '@vitejs/plugin-react-swc';
 import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
 
-import dts from 'vite-plugin-dts';
+// const a = Object.fromEntries(
+// 	glob
+// 		.sync('lib/**/index.{ts,tsx}', {
+// 			ignore: ['lib/**/*.d.ts']
+// 		})
+// 		.map((file) => [
+// 			relative('lib', file.slice(0, file.length - extname(file).length)),
+// 			fileURLToPath(new URL(file, import.meta.url))
+// 		])
+// );
+
+// console.log('D:/project/front-end/react/react-component-lib/lib/components/NavigationBar/index.ts');
+
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), dts({ rollupTypes: true })],
+	plugins: [react(), tailwindcss()],
 	resolve: {
 		alias: {
 			'@styles': path.resolve(__dirname, './src/styles'),
 			'@components': path.resolve(__dirname, './lib/components'),
-			'@pages': path.resolve(__dirname, './src/pages')
+			'@pages': path.resolve(__dirname, './src/pages'),
+			'@lib': path.resolve(__dirname, './lib')
 		}
 	},
 	build: {
 		lib: {
-			entry: resolve(__dirname, 'lib/index.ts')
+			entry: resolve(__dirname, 'lib/index.ts'),
+			formats: ['es']
 		},
 		rollupOptions: {
-			external: ['react', 'react/jsx-runtime', 'react-dom'],
-			output: [
-				{
-					dir: './dist',
-					format: 'es'
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM'
 				}
-			]
+			}
 		},
-		sourcemap: true,
+
 		copyPublicDir: false
 	}
 });
